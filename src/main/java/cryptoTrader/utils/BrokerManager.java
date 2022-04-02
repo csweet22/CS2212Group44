80d879455e2e33c90d51b3ec7cd8554069063366
Shmoogus <walker.mitchell44@gmail.com>
2022-04-02 16:10:28 -0400

    Added Brokers and Broker manager

---
+++ src/main/java/cryptoTrader/utils/BrokerManager.java
@@ -0,0 +1,48 @@
+package cryptoTrader.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class BrokerManager {
+
+    private static BrokerManager manager = null;
+    private HashMap<String, Broker> brokers = new HashMap<>();
+
+    private BrokerManager(){
+
+    }
+
+    public static BrokerManager getInstance(){
+        if (manager == null){
+            manager = new BrokerManager();
+        }
+        return manager;
+    }
+
+    public void clearBrokers(){
+        brokers.clear();
+    }
+
+    //returns true if the broker was added
+    public boolean addBroker(String name, String[] coins, String strategyName){
+        if (brokers.get(name) != null){
+            return false;
+        }
+        Strategy strategy = StrategyFactory.getInstance().getStrategy(strategyName);
+        brokers.put(name, new Broker(name, coins, strategy));
+        return true;
+    }
+
+    public ArrayList<Broker> getBrokers(){
+        ArrayList<Broker> brokerList = new ArrayList<>();
+        for (Broker broker: brokers.values()){
+            brokerList.add(broker);
+        }
+        return brokerList;
+    }
+
+    public void printBrokers(){
+        System.out.println(brokers);
+    }
+    
+}
