714febcdb3f8cc9e6d9befe0250fb784980950d8
Shmoogus <walker.mitchell44@gmail.com>
2022-04-04 14:49:58 -0400

    Added comments and 'none' fix

--- src/main/java/cryptoTrader/utils/Strategy.java
+++ src/main/java/cryptoTrader/utils/Strategy.java
@@ -2,7 +2,23 @@ package cryptoTrader.utils;
 
 import java.util.HashMap;
 
+/**
+ * An interface that all strategies must inherit to ensure functionality
+ * @author Mitchell Walker
+ */
 interface Strategy {
-    public Trade trade(String broker, String[] coins,HashMap<String,Double> coin_price, String date);
+    /**
+     * A method where the logic for performing trades must be implemented
+     * @param broker name of the broker making the trade
+     * @param coins a list of coin names the broker is interested in
+     * @param coin_price a hashmap that stores prices of coins using their names as keys
+     * @param date the date of the trade
+     * @return a trade object containing all the information about a trade when it is performed
+     */
+    public Trade trade(String broker, String[] coins,HashMap<String,Double> coin_price, String date); 
+
+    /**
+     * @return the name of the strategy
+     */
     public String getName();
 }
